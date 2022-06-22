39ed97ae5792a31a7d7718641c3f5a2d176850b0
tobozo <tobozo@users.noreply.github.com>
2022-06-22 20:14:36 +0200

    added self-update

--- settings.h
+++ settings.h
@@ -28,3 +28,4 @@
 #define TRANSVERTER_LO_4    406000000           // LO Frequency if you're using a Transverter
 #define TRANSVERTER_LO_5   9968000000           // LO Frequency if you're using a Transverter
 
+#define UPDATER_URL "https://tobozo.github.io/ICSMeter/"
